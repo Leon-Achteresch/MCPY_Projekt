b8fc3c92409bb7744f2046664671b9a5b5066d96
FSchluenkes <frederik@schluenkes.de>
2023-09-27 14:19:13 +0200

    Aufgabe 2 berichtigt

--- Aufgabe2.py
+++ Aufgabe2.py
@@ -2,33 +2,35 @@ import keyboard
 from mcpi.minecraft import Minecraft
 
 class Wall:
-    def __init__(self, pos, rotated=False, material_id="Default:stone", Width=5, height=5):
-        self.__mc = Minecraft.create()
+    def __init__(self, pos, bw:Minecraft, rotated=False, material_id="Default:stone", width=5, height=5):
+        self.__mc:Minecraft = bw
         self.pos = pos
         self.rotated = rotated
         self.material_id = material_id
-        self.Width = Width
+        self.width = width
         self.height = height
 
     def build(self):
         print('build')
+        self.__mc.postToChat('building')
         x, y, z = self.pos
         if self.rotated:
             for i in range(self.height):
-                for j in range(self.Width):
-                    self.__mc.setBlock(x + j, y + i, z, self.material_id)
+                for j in range(self.width):
+                    self.__mc.setBlock(x +1 + j, y + i, z, self.material_id)
         else:
-            for i in range(self.Width):
+            for i in range(self.width):
                 for j in range(self.height):
-                    self.__mc.setBlock(x, y + i, z + j, self.material_id)
+                    self.__mc.setBlock(x, y + i, z +1 + j, self.material_id)
 
 def execAufgabe2():
-    def build():
+    def build(mc:Minecraft):
         player_pos = mc.player.getPos()
-        rotated_wall = Wall((player_pos.x +1 , player_pos.y, player_pos.z), rotated=True, material_id=1, Width=5, height=5)
-        rotated_wall.build()
-        not_rotated_wall = Wall((player_pos.x, player_pos.y, player_pos.z +1), rotated=False, material_id=1, Width=5, height=5)
-        not_rotated_wall.build()
+        wall = Wall((player_pos.x , player_pos.y, player_pos.z), rotated=True, material_id=1, width=5, height=5, bw = mc)
+        wall.build()
+        wall.rotated=False
+        wall.build()
+        
 
     mc = Minecraft.create(address="localhost", port=4711)
     
@@ -37,7 +39,7 @@ def execAufgabe2():
     # Erstelle eine gedrehte Wand
     
     
-    keyboard.on_press_key('b', lambda e: build())
+    keyboard.on_press_key('b', lambda e: build(mc))
 
     # Halte das Programm am Laufen
     keyboard.wait()
