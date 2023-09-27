2ea645eea051fee230c60dacb62cf98cde3250e6
FSchluenkes <frederik@schluenkes.de>
2023-09-27 14:00:22 +0200

    Augabe 2 fixxed

--- Aufgabe2.py
+++ Aufgabe2.py
@@ -3,7 +3,7 @@ from mcpi.minecraft import Minecraft
 
 class Wall:
     def __init__(self, pos, rotated=False, material_id="Default:stone", Width=5, height=5):
-        self.mc = Minecraft.create()
+        self.__mc = Minecraft.create()
         self.pos = pos
         self.rotated = rotated
         self.material_id = material_id
@@ -11,25 +11,33 @@ class Wall:
         self.height = height
 
     def build(self):
+        print('build')
         x, y, z = self.pos
-        material_id = self.mc.getBlockWithData(x, y, z).id
         if self.rotated:
             for i in range(self.height):
                 for j in range(self.Width):
-                    self.mc.setBlock(x + j, y, z + i, material_id)
+                    self.__mc.setBlock(x + j, y + i, z, self.material_id)
         else:
             for i in range(self.Width):
                 for j in range(self.height):
-                    self.mc.setBlock(x + i, y, z + j, material_id)
+                    self.__mc.setBlock(x, y + i, z + j, self.material_id)
 
 def execAufgabe2():
+    def build():
+        player_pos = mc.player.getPos()
+        rotated_wall = Wall((player_pos.x +1 , player_pos.y, player_pos.z), rotated=True, material_id=1, Width=5, height=5)
+        rotated_wall.build()
+        not_rotated_wall = Wall((player_pos.x, player_pos.y, player_pos.z +1), rotated=False, material_id=1, Width=5, height=5)
+        not_rotated_wall.build()
+
     mc = Minecraft.create(address="localhost", port=4711)
-    player_pos = mc.player.getPos()
+    
+    mc.postToChat("Warte auf input...")
     print("Mit Server verbunden. Warte auf Input...")
     # Erstelle eine gedrehte Wand
-    rotated_wall = Wall((player_pos.x, player_pos.y, player_pos.z), rotated=True, material_id="minecraft:stone", Width=5, height=5)
-
-    keyboard.on_press_key('b', lambda e: rotated_wall.build())
+    
+    
+    keyboard.on_press_key('b', lambda e: build())
 
     # Halte das Programm am Laufen
     keyboard.wait()
